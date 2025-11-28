44171ada1aec50424772f37e91e87203787a5ff5
Osier Nono <osiernono@gmail.com>
2025-11-28 05:05:40 +0000

    Implimenting my quack code

--- main.cpp
+++ main.cpp
@@ -65,9 +65,96 @@ template <typename T>
 
 class Quack{
     private:
+    struct Node {
+        T data;
+        Node* next;
+        Node* prev;
+        Node(const T& d) : data(d), next(nullptr), prev(nullptr) {}
+    };
+
+    Node* head;
+    Node* tail;
 
     public:
-    
+
+    Quack() : head(nullptr), tail(nullptr) {}
+    ~Quack() {
+        while (!isEmpty()) {
+            popFront();
+        }
+    }
+
+    bool isEmpty() const {
+        return head == nullptr;
+    }
+
+    void pushFront(const T& val) {
+        Node* newNode = new Node(val);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+
+    void pushBack(const T& val) {
+        Node* newNode = new Node(val);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+
+    T popFront() {
+        if (isEmpty()) {
+            throw runtime_error("Quack is empty");
+        }
+        Node* temp = head;
+        T val = head->data;
+        head = head->next;
+        if (head) {
+            head->prev = nullptr;
+        } else {
+            tail = nullptr;
+        }
+        delete temp;
+        return val;
+    }
+
+    T popBack() {
+        if (isEmpty()) {
+            throw runtime_error("Quack is empty");
+        }
+        Node* temp = tail;
+        T val = tail->data;
+        tail = tail->prev;
+        if (tail) {
+            tail->next = nullptr;
+        } else {
+            head = nullptr;
+        }
+        delete temp;
+        return val;
+    }
+
+    T front() const {
+        if (isEmpty()) {
+            throw runtime_error("Quack is empty");
+        }
+        return head->data;
+    }
+
+    T back() const {
+        if (isEmpty()) {
+            throw runtime_error("Quack is empty");
+        }
+        return tail->data;
+    }
 };
 
 
